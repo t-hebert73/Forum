b2512bda0a93223bb072eea419bf248b6910f2a7
t.hebert73 <t.hebert73@gmail.com>
2013-04-17 00:24:38 -0400

    Revert "Revert "first commit""
    This reverts commit b20d2c79d97a19f47ca085a6c85b9fbda2298f02.

---
+++ src/connect.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * filename: connect.php
+ * last edited: April 16, 2013
+ * authors: Trevor Hebert, Miguel Mawyin
+ * file description: This file connects to the database
+ */
+
+//This code connects to the database.
+mysql_connect('localhost','root','root')or die('Unable to connect to database'); 
+mysql_select_db('forum')or die('Unable to select the forum database'); 
+//======================================================================
+
+?>
---
+++ src/footer.php
@@ -0,0 +1,29 @@
+<!--
+ * filename: footer.php
+ * last edited: April 16, 2013
+ * authors: Trevor Hebert, Miguel Mawyin
+ * file description: This is the footer file that closes the body
+ *
+ * ->
+ </div> <!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="http://twitter.github.io/bootstrap/assets/js/jquery.js"></script>
+    <script src="http://twitter.github.io/bootstrap/assets/js/bootstrap-transition.js"></script>
+    <script src="http://twitter.github.io/bootstrap/assets/js/bootstrap-alert.js"></script>
+    <script src="http://twitter.github.io/bootstrap/assets/js/bootstrap-modal.js"></script>
+    <script src="http://twitter.github.io/bootstrap/assets/js/bootstrap-dropdown.js"></script>
+    <script src="http://twitter.github.io/bootstrap/assets/js/bootstrap-scrollspy.js"></script>
+    <script src="http://twitter.github.io/bootstrap/assets/js/bootstrap-tab.js"></script>
+    <script src="http://twitter.github.io/bootstrap/assets/js/bootstrap-tooltip.js"></script>
+    <script src="http://twitter.github.io/bootstrap/assets/js/bootstrap-popover.js"></script>
+    <script src="http://twitter.github.io/bootstrap/assets/js/bootstrap-button.js"></script>
+    <script src="http://twitter.github.io/bootstrap/assets/js/bootstrap-collapse.js"></script>
+    <script src="http://twitter.github.io/bootstrap/assets/js/bootstrap-carousel.js"></script>
+    <script src="http://twitter.github.io/bootstrap/assets/js/bootstrap-typeahead.js"></script>
+	<script src="http://code.jquery.com/jquery.js"></script>
+	
+  </body>
+</html>
